809e6d9cbd47da0f1a0f11c1dd6a145654867da8
G-a-b-b-s <gaba.czapska@onet.pl>
2024-05-24 12:08:07 +0200

    Initial commit

---
+++ CheapFlightsAppProject/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CheapFlightsAppProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
